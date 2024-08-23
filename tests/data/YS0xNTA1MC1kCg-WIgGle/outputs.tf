f5d6f875fcddfc71ed90c1efc9476a42ec878c47
matttrach <matt.trachier@suse.com>
2024-08-22 21:47:32 -0500

    fix: remove test data
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- tests/data/YS0xNTA1MC1kCg-WIgGle/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "server" {
-  value = module.this.server
-}
-output "image" {
-  value = module.this.image
-}
-output "access" {
-  value = module.access
-}
--- tests/data/YS0zMDQyOS1kCg-2Hnz2r/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "identifier" {
-  type = string
-}
